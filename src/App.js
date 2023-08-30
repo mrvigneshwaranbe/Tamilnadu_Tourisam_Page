51bf1316823b9fbb1a1b1c0db6ac2075a21957a1
mrvigneshwaranbe <139253412+mrvigneshwaranbe@users.noreply.github.com>
2023-08-30 13:44:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import PlaceToVisit from "./components/places/PlaceToVisit";
+import TimeToVisit from "./components/timeToVisit/places/TimeToVisit";
 import Home from "./components/Home";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/place-to-visit" element={<PlaceToVisit />} />
+        <Route path="/timeToVisit" element={<TimeToVisit />} />
       </Routes>
     </BrowserRouter>
   );
